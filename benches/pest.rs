c57b0b2c73c045866e919b6389d41bb7d62ff59e
Geoffroy Couprie <geo.couprie@gmail.com>
2017-08-24 15:33:00 +0200

    initial import

---
+++ benches/pest.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+extern crate test;
+
+extern crate pest;
+extern crate pest_grammars;
+
+use test::Bencher;
+
+use pest::Parser;
+use pest_grammars::json;
+
+const CANADA : &str = include_str!("../assets/canada.json");
+
+#[bench]
+fn pest_json(b: &mut Bencher) {
+  b.iter(||{
+    json::JsonParser::parse_str(json::Rule::json, CANADA).unwrap_or_else(|e| panic!("{}", e))
+  });
+
+}
